31a9419d4cff7bb0c5fa0f84035ae14569264248
lambent41 <lambent41@gmail.com>
2021-01-20 16:42:20 +0900

    01/20 16:42 cw

--- master/NodeMaster/NodeMaster.java
+++ master/NodeMaster/NodeMaster.java
@@ -1,5 +1,3 @@
-package master;
-
 import bloomfilter.AbstractBloomFilter;
 import bloomfilter.BasicBloomFilter;
 import bloomfilter.BloomFilter;
@@ -10,11 +8,11 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Master {
+public class NodeMaster {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);   // System.in은 모든 Scanner가 공유하므로 하나의 Scanner를 쓰는 것이 좋다.
         System.out.println("Type Number of Expected Elements to be Inserted in the Filter");
         int expected_elements = scanner.nextInt();
         System.out.println("Type Desired False Positive Probability");
@@ -32,12 +30,13 @@ public class Master {
             System.out.println("||1. Insert || 2. Read || 3. Quit||");
             System.out.println("==================================================");
             boolean brk = false;
-            switch (scanner.nextInt()) {
+            int slct = scanner.nextInt();
+            switch (slct) {
                 case 1:
-                    implement_insert(bloomFilter, malleDB);
+                    implement_insert(bloomFilter, malleDB, scanner);
                     break;
                 case 2:
-                    
+                    implement_read(bloomFilter, malleDB, scanner);
                     break;
                 case 3:
                     brk=true;
@@ -61,14 +60,16 @@ public class Master {
     private static void activate(String node_num){
         // 
     }
+    
+    private static void notify_to_usr(String message){
+        //
+    }
 
-    private static void implement_insert(BloomFilter bloomFilter, MalleDB malleDB){
-        Scanner scanner_ins = new Scanner(System.in);
-
-        System.out.println("Put the Filename");
-        String filename = scanner_ins.next();
+    private static void implement_insert(BloomFilter bloomFilter, MalleDB malleDB, Scanner scanner){
+        System.out.println("Put the Filenam to insert");
+        String filename = scanner.next();
         System.out.println("Put the Unique Key");
-        String unique_key = scanner_ins.next(); // PROGRESS #1
+        String unique_key = scanner.next(); // PROGRESS #1
 
         String hash_input = filename + unique_key;
         bloomFilter.addData(hash_input);    // PROGRESS #2
@@ -80,6 +81,21 @@ public class Master {
         activate(node_num); // PROGRESS #5
     }
 
+    private static void implement_read(BloomFilter bloomFilter, MalleDB malleDB, Scanner scanner){
+        System.out.println("Put the Filename to read");
+        String filename = scanner.next();
+        System.out.println("Put the Unique Key");
+        String unique_key = scanner.next(); // PROGRESS #1
+        
+        String hash_input = filename + unique_key;
+        if(bloomFilter.isPresent(hash_input)){  // PROGRESS #2
+            String search_node; // = malleDB.read(filename);   // PROGRESS #3 MalleDB는 read시 출력밖에 하지 않음... 수정필요
+            activate(search_node);  // PROGRESS #4
+        }
+        else{    // PROGRESS #3-FAIL
+            notify("FAIL");
+        }
+    }
     // private static void testWithOptimumSizeBitSet() {
     //     Arrays.stream(randomWords).forEach(bloomFilter::addData);
 

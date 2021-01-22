647187e18d5643cbcd8092f8b5358ebdf3ae797f
lambent41 <lambent41@gmail.com>
2021-01-22 15:39:32 +0900

    01/22 15:39 cw

--- master/NodeMaster/NodeMaster.java
+++ master/NodeMaster/NodeMaster.java
@@ -4,21 +4,19 @@ import bloomfilter.BloomFilter;
 import db.MalleDB;
 import util.Options;
 
-import java.util.Scanner;
 import java.util.Arrays;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+
+
 public class NodeMaster {
+    // Default Values
+    int expected_elements = 1000000;
+    double desired_p = 0.01;
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);   // System.in은 모든 Scanner가 공유하므로 하나의 Scanner를 쓰는 것이 좋다.
-        System.out.println("Type Number of Expected Elements to be Inserted in the Filter");
-        int expected_elements = scanner.nextInt();
-        System.out.println("Type Desired False Positive Probability");
-        double desired_p = scanner.nextDouble();
 
         MalleDB malleDB = new MalleDB();
         Options option = new Options(Options.DB_TYPE.LEVELDB, Options.DB_TYPE.LEVELDB, Options.DB_TYPE.LEVELDB);
@@ -28,9 +26,6 @@ public class NodeMaster {
         BloomFilter bloomFilter = new BasicBloomFilter(desired_p, expected_elements);
 
         System.out.println("==================Waiting For Request===================");
-        while(wait_for_req()){
-            boolean brk = false;
-            int slct = scanner.nextInt();
             switch (slct) {
                 case 1:
                     insert(bloomFilter, malleDB, scanner);
@@ -45,16 +40,8 @@ public class NodeMaster {
                     System.out.println("Wrong Number Typed.");
                     break;
             }
-            if(brk) break;
         }
 
-    scanner.close();
-
-    }
-
-    private static boolean wait_for_req(){
-        // wait for REQUEST
-        return true;
     }
 
     private static String calculate(){
@@ -70,11 +57,7 @@ public class NodeMaster {
         System.out.println("Notify to User that the file is not in DB");
     }
 
-    private static void insert(BloomFilter bloomFilter, MalleDB malleDB, Scanner scanner){
-        System.out.println("Getting Filename from User");
-        String filename = scanner.next();
-        System.out.println("Getting Unique Key from User");
-        String unique_key = scanner.next(); // PROCESS #1
+    private static void insert(BloomFilter bloomFilter){
 
         String hash_input = filename + unique_key;
         bloomFilter.addData(hash_input);    // PROCESS #2

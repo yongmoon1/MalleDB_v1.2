b4ac1b0f8be96118d055ba73b32d783f9de218ff
lambent41 <lambent41@gmail.com>
2021-02-04 17:38:04 +0900

    2/4 17:38 cw

---
+++ new/test.java
@@ -0,0 +1,14 @@
+import db.MalleDB;
+import util.Options;
+
+public class test {
+    public static void main(String[] args){
+        MalleDB malleDB = new MalleDB();
+        malleDB.init(new Options(Options.DB_TYPE.LEVELDB, Options.DB_TYPE.LEVELDB, Options.DB_TYPE.LEVELDB));
+        malleDB.create();
+
+        malleDB.insert("filename", "1");
+        malleDB.read("filename");
+    }
+}
+
